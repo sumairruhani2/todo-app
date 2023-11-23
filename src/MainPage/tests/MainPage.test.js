5847261ee188b4be074c8f833c899d2f9e1d3e8e
Abdul-SubhanL <AbdulSubhan.Latif@Arup.com>
2023-11-23 13:05:08 +0000

    configured husky

--- src/MainPage/tests/MainPage.test.js
+++ src/MainPage/tests/MainPage.test.js
@@ -1,24 +1,25 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MainPage from '../MainPage';
 
 beforeEach(() => {
   render(<MainPage />);
-})
+});
 
 describe('MainPage', () => {
   test('renders Add Todo button', () => {
     const addButton = screen.getByText(/Add Todo/i);
     expect(addButton).toBeInTheDocument();
   });
-  
+
   test('renders New Todo input field', () => {
     const inputField = screen.getByLabelText(/New Todo/i);
     expect(inputField).toBeInTheDocument();
   });
-  
+
   test('renders correct title', () => {
     const titleElement = screen.queryByTestId('main-page-title'); // Add a data-testid
     expect(titleElement).toBeInTheDocument();
-    expect(titleElement).toHaveStyle({ fontSize: '50px' }); 
+    expect(titleElement).toHaveStyle({ fontSize: '50px' });
   });
-})
+});
