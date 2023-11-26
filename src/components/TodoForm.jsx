ad2c713b7a16e342c5ca5ac41f4a2a85f5155cee
sumairruhani2 <sumair.ruhani@ada.ac.uk>
2023-11-26 16:54:32 +0000

    added styling

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,24 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
 
 // Styles
 import { Button, Box, TextField } from '@mui/material';
 
-// hooks
-import { useState } from 'react';
-
 export default function TodoForm({ addTodo }) {
   const [text, setText] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (text.trim() === '') {
+      setError('Todo item cannot be empty');
+      return;
+    }
+
     addTodo(text);
     setText('');
+    setError('');
   };
 
   return (
@@ -27,9 +32,29 @@ export default function TodoForm({ addTodo }) {
         name="todo"
         autoFocus
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          setText(e.target.value);
+          setError('');
+        }}
+        error={Boolean(error)}
+        helperText={error}
       />
-      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{
+          mt: 3,
+          mb: 2,
+          bgcolor: '#e84118',
+          '&:hover': {
+            bgcolor: '#c23616',
+          },
+          '&:active': {
+            bgcolor: '#c23616',
+          },
+        }}
+      >
         Add Todo
       </Button>
     </Box>
